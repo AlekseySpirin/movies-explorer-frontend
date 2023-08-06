9e4be61aad5fc00284ba3a43bf7986b692a5e454
AlekseySpirin <spirin.mezhmun@yandex.ru>
2023-08-06 12:22:53 +0300

    CSS: Techs

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -3,25 +3,27 @@ import './Techs.css';
 
 function Techs() {
   return (
-    <div id={'techs'} className={'techs'}>
-      <h2 className={'title'}>Технологии</h2>
-      <hr className={'hr'} />
-      <div className={'techs__wrapper'}>
-        <h3 className={'techs__subtitle'}>7 технологий</h3>
-        <p className={'techs__text'}>
-          На курсе веб-разработки мы освоили технологии, которые применили в
-          дипломном проекте.
-        </p>
+    <div className='techs_bg_grey'>
+      <div id={'techs'} className={'techs'}>
+        <h2 className={'title'}>Технологии</h2>
+        <hr className={'hr'} />
+        <div className={'techs__wrapper'}>
+          <h3 className={'techs__subtitle'}>7 технологий</h3>
+          <p className={'techs__text'}>
+            На курсе веб-разработки мы освоили технологии, которые применили в
+            дипломном проекте.
+          </p>
+        </div>
+        <ul className={'techs-list'}>
+          <li className={'techs-list__item'}>HTML</li>
+          <li className={'techs-list__item'}>CSS</li>
+          <li className={'techs-list__item'}>JS</li>
+          <li className={'techs-list__item'}>React</li>
+          <li className={'techs-list__item'}>Git</li>
+          <li className={'techs-list__item'}>Express.js</li>
+          <li className={'techs-list__item'}>mongoDB</li>
+        </ul>
       </div>
-      <ul className={'techs-list'}>
-        <li className={'techs-list__item'}>HTML</li>
-        <li className={'techs-list__item'}>CSS</li>
-        <li className={'techs-list__item'}>JS</li>
-        <li className={'techs-list__item'}>React</li>
-        <li className={'techs-list__item'}>Git</li>
-        <li className={'techs-list__item'}>Express.js</li>
-        <li className={'techs-list__item'}>mongoDB</li>
-      </ul>
     </div>
   );
 }
