2bee58f55e85136ac914994e11d98a91e4dcad5b
AlekseySpirin <spirin.mezhmun@yandex.ru>
2023-08-06 13:45:03 +0300

    per-pixel: remains (AboutMe, Portfolio, Footer

--- src/components/NavTab/NavTab.jsx
+++ src/components/NavTab/NavTab.jsx
@@ -3,30 +3,28 @@ import cl from './NavTab.module.css';
 
 function NavTab() {
   return (
-    <div>
-      <nav className={cl.navTab}>
-        <ul className={cl.navTabList}>
-          <li className={cl.navTabItem}>
-            <a className={cl.navTabLink} href='#about-project'>
-              О проекте
-            </a>
-          </li>
-          <li className={cl.navTabItem}>
-            <a className={cl.navTabLink} href='#techs'>
-              Технологии
-            </a>
-          </li>
-          <li className={cl.navTabItem}>
-            <a className={cl.navTabLink} href='#about-me'>
-              Студент
-            </a>
-          </li>
-        </ul>
-        {/* <button type={'button'}>О проекте</button> */}
-        {/* <button type={'button'}>Технологии</button> */}
-        {/* <button type={'button'}>Студент</button> */}
-      </nav>
-    </div>
+    <nav className={cl.navTab}>
+      <ul className={cl.navTabList}>
+        <li className={cl.navTabItem}>
+          <a className={cl.navTabLink} href='#about-project'>
+            О проекте
+          </a>
+        </li>
+        <li className={cl.navTabItem}>
+          <a className={cl.navTabLink} href='#techs'>
+            Технологии
+          </a>
+        </li>
+        <li className={cl.navTabItem}>
+          <a className={cl.navTabLink} href='#about-me'>
+            Студент
+          </a>
+        </li>
+      </ul>
+      {/* <button type={'button'}>О проекте</button> */}
+      {/* <button type={'button'}>Технологии</button> */}
+      {/* <button type={'button'}>Студент</button> */}
+    </nav>
   );
 }
 
