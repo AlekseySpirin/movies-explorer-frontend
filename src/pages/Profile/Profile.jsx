d858f24b1b6118e5c50d5ad24a0798c6078cbd42
AlekseySpirin <spirin.mezhmun@yandex.ru>
2023-08-13 19:29:49 +0300

    per-pixel fix: Profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -6,24 +6,29 @@ function Profile() {
   return (
     <div className={'profile'}>
       <Navigation />
-      <h2 className='profile__title'>Привет, Алексей!</h2>
-      <ul className='profile-list'>
-        <li className='profile-list__item'>
-          <p>Имя</p>
-          <p>Алексей</p>
-        </li>
-        <li className='profile-list__item'>
-          <p>E-mail</p>
-          <p>pochta@yandex.ru</p>
-        </li>
-      </ul>
-      <div className='profile__btn-container'>
-        <button type={'button'} className='profile__btn'>
-          Редактировать
-        </button>
-        <button type={'button'} className='profile__btn profile__btn_color_red'>
-          Выйти из аккаунта
-        </button>
+      <div className='profile__wrapper'>
+        <h2 className='profile__title'>Привет, Алексей!</h2>
+        <ul className='profile-list'>
+          <li className='profile-list__item'>
+            <p>Имя</p>
+            <p>Алексей</p>
+          </li>
+          <li className='profile-list__item'>
+            <p>E-mail</p>
+            <p>pochta@yandex.ru</p>
+          </li>
+        </ul>
+        <div className='profile__btn-container'>
+          <button type={'button'} className='profile__btn'>
+            Редактировать
+          </button>
+          <button
+            type={'button'}
+            className='profile__btn profile__btn_color_red'
+          >
+            Выйти из аккаунта
+          </button>
+        </div>
       </div>
     </div>
   );
